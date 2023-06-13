c6602cbcaec996b5040c5a2715c563b4c1e47597
Augusto Cezar Ribeiro Santana <cezaodabahia@gmail.com>
2023-06-13 15:01:54 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/fiap/techchallenge/model/Usuario.kt
@@ -0,0 +1,14 @@
+package br.com.fiap.techchallenge.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "usuarios")
+data class UsuarioModel(
+    @Id
+    val id: Long,
+    val nome: String,
+    val email: String
+)
