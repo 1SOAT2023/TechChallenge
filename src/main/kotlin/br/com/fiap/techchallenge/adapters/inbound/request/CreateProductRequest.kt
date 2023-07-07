9cd05e8ff09afc27a83ad7c6d1cb5cb45d04d4e6
cezaodabahia <cezaodabahia@gmail.com>
2023-07-06 23:13:39 -0300

    tipo de dado do productType trocado de String para ProductType, para que o enum apareça na documentação do swagger informando os possíveis valores para o campo

--- src/main/kotlin/br/com/fiap/techchallenge/adapters/inbound/request/CreateProductRequest.kt
+++ src/main/kotlin/br/com/fiap/techchallenge/adapters/inbound/request/CreateProductRequest.kt
@@ -7,9 +7,9 @@ import java.math.BigDecimal
 data class CreateProductRequest(
     val title: String,
     val sku: String,
-    val productType: String,
+    val productType: ProductType,
     val description: String,
     val value: BigDecimal
 ) {
-    fun toProduct() = Product(title = title, productType = ProductType.valueOf(productType), description = description, sku = sku, value=value)
+    fun toProduct() = Product(title = title, productType = productType, description = description, sku = sku, value=value)
 }
